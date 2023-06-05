a81a8d712bec0b79545d085ed74b54960dbbef5e
Goodnews Adewole <goodnews.adewole@example.com>
2023-06-05 16:37:14 +0100

    day 1 done

--- landing-page/src/home/components/Navbar.jsx
+++ landing-page/src/home/components/Navbar.jsx
@@ -11,22 +11,38 @@ const styles = {
 
 function Navbar(){
     return(
-    <div style={styles} className="navbar">
-        <div className="logo">
-         <img src="logo.svg"/>
+    <div>
+        <div style={styles} className="navbar">
+            <div className="logo">
+            <img src="logo.svg"/>
+            </div>
+            
+            <div className="links">
+                <a>Home</a>
+                <a>About</a>
+                <a>Become an affliate</a>
+                <a>Faq</a>
+                <a>Contact Us</a>
+            </div>
+            <div className="nav-btn">
+                <button type="button" className="button1">Affliate Dashboard</button>
+                <button type="button" className="button2">Download App</button>
+            </div>
         </div>
-        
-        <div className="links">
-            <a>Home</a>
-            <a>About</a>
-            <a>Become an affliate</a>
-            <a>Faq</a>
-            <a>Contact Us</a>
+        <div className="nav-content">
+            <div className="text">
+                <p>You might be miles away from home but getting money across to your loved ones shouldn't be a hassle</p>
+                <button type="button">
+                    <p>Get it on</p>
+                    <p>Google Play</p>
+                </button>
+                <button type="button">
+                    <p>Download on the</p>
+                    <p>App Store</p>
+                </button>
+            </div>
         </div>
-        <div className="nav-btn">
-            <button type="button" className="button1">Affliate Dashboard</button>
-            <button type="button" className="button2">Download App</button>
-        </div>
+        <div></div>
     </div>
     )
 }
