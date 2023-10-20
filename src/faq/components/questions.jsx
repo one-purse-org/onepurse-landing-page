8c8a75e750fdc9f596ee8a63b0052a27b1b81afc
Goodnews Adewole <goodnews.adewole@example.com>
2023-10-20 12:25:58 +0100

    fixed faq bug

--- src/faq/components/questions.jsx
+++ src/faq/components/questions.jsx
@@ -1,13 +1,25 @@
 import "../../styles/faq/question.css"
 import questions from "./faqQuestion";
+import { useState } from "react";
 
 function Accordion(props){
-
+    let scrolled = false
+    function scrollUpOrDown(){
+        let scroll = window.scrollY
+        if(scrolled === false){
+            window.scrollTo(scroll, scroll + 70)
+            scrolled = true
+            return true
+        }
+        window.scrollTo(scroll, scroll - 70)
+        scrolled = false
+        return true
+    }
     return(
         <div className="accordion accordion-flush">
             <div className="accordion-item">
                 <h2 className="accordion-header">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#" + props.target}>
+                <button onClick={scrollUpOrDown} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#" + props.target}>
                     {props.head}
                 </button>
                 </h2>
@@ -23,26 +35,6 @@ function Accordion(props){
 }
 
 function Questions(){
-    
-   
-
-    function handleScroll(){
-        let questionsContainer = document.querySelector(".question-container");
-        let scrolled = questionsContainer.scrollTop;
-
-        let questionContainerOne = document.querySelector(".first-question");
-        let questionContainerTwo = document.querySelector(".second-question");
-        let questionContainerThree = document.querySelector(".third-question");
-    
-        let questionContainerOneHeight = questionContainerOne.clientHeight;
-        let questionContainerTwoHeight = questionContainerTwo.clientHeight;
-        let questionContainerThreeHeight = questionContainerThree.clientHeight;
-
-        scrolled < questionContainerOneHeight && performChange()
-        scrolled >= questionContainerOneHeight && performChange("one");
-        scrolled >= questionContainerOneHeight + questionContainerTwoHeight - 10 && performChange("two")
-    }
-
     function performChange(theScrolledOne){
         let questionContainerOne = document.querySelector(".faqQuestionOne");
         let questionContainerTwo = document.querySelector(".faqQuestionTwo");
@@ -64,25 +56,19 @@ function Questions(){
 
     function handleClick(number){
         let questionsContainer = document.querySelector(".question-container");
-
         if (number==="one"){
-            let questionContainerHeight = 0
-            questionsContainer.scrollTop = questionContainerHeight
+            setQuestion(questions["accountSetup"])
             performChange()
         }else  if (number==="two"){
-            let questionContainer = document.querySelector(".first-question");
-            let questionContainerHeight = questionContainer.clientHeight
-            questionsContainer.scrollTop = questionContainerHeight
+            setQuestion(questions["security"])
             performChange("one")
         }else  if (number==="three"){
-            let questionContainerOne = document.querySelector(".second-question");
-            let questionContainerTwo = document.querySelector(".first-question")
-            let questionContainerHeight = questionContainerOne.clientHeight + questionContainerTwo.clientHeight
-            questionsContainer.scrollTop = questionContainerHeight
+            setQuestion(questions["sendingMoney"])
             performChange("two")
         }   
     }
-
+    let [useQuestions, setQuestion] = useState(questions["accountSetup"])
+    
     return(
         <div className="questions">
             <div className="container questions-container">
@@ -91,16 +77,8 @@ function Questions(){
                     <p onClick={()=>{handleClick("two")}} className='faqQuestions faqQuestionTwo'>Sending Money</p>
                     <p onClick={()=>{handleClick("three")}} className='faqQuestions faqQuestionThree'>Security</p>
                 </div>
-                <div className="question-container" onScroll={handleScroll}>
-                    <div className="first-question">
-                        {questions.accountSetup.map(question=> <Accordion key={question.key} head={question.head} body={question.body} target={question.target} /> )}
-                    </div>
-                    <div className="second-question">
-                        {questions.sendingMoney.map(question=> <Accordion key={question.key} head={question.head} body={question.body} target={question.target} /> )}
-                    </div>
-                    <div className="third-question">
-                        {questions.security.map(question=> <Accordion key={question.key} head={question.head} body={question.body} target={question.target} /> )}
-                    </div>
+                <div className="question-container">
+                    {useQuestions.map(question=> <Accordion key={question.key} head={question.head} body={question.body} target={question.target} /> )}
                 </div>
             </div>
         </div>
